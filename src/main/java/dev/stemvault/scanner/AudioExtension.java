78f5feb0eda7365c7668f17d4559891f305bd867
nicodamn <nikosfikos@icloud.com>
2026-04-04 07:47:08 +0200

    Audio extensions are now in enum B)

---
+++ src/main/java/dev/stemvault/scanner/AudioExtension.java
@@ -0,0 +1,18 @@
+package dev.stemvault.scanner;
+
+public enum AudioExtension {
+    MP3(".mp3"),
+    WAV(".wav"),
+    FLAC(".flac"),
+    OGG(".ogg");
+
+    private final String extension;
+
+    AudioExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+}
--- src/main/java/dev/stemvault/scanner/FileScanner.java
+++ src/main/java/dev/stemvault/scanner/FileScanner.java
@@ -7,25 +7,17 @@ import java.util.ArrayList;
 
 public class FileScanner {
 
-    private ArrayList<String> extensions;
-
-    public FileScanner(ArrayList<String> extensions) {
-        this.extensions = extensions;
-    }
-
-    private ArrayList<Path> paths = new ArrayList<>();
-
     public ArrayList<Path> scan(Path path) throws IOException {
+        ArrayList<Path> paths = new ArrayList<>();
         SimpleFileVisitor<Path> visitor = new SimpleFileVisitor<Path>() {
             @Override
             public FileVisitResult visitFile(
                 Path file,
                 BasicFileAttributes attrs
             ) throws IOException {
-                for (String extension : extensions) {
-                    String tmp = file.getFileName().toString();
-                    tmp = tmp.toLowerCase();
-                    if (tmp.endsWith(extension)) {
+                for (AudioExtension extension : AudioExtension.values()) {
+                    String tmp = file.getFileName().toString().toLowerCase();
+                    if (tmp.endsWith(extension.getExtension())) {
                         paths.add(file);
                     }
                 }
