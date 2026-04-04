76965a73396e3963bf0f23a376212934ae238a45
nicodamn <nikosfikos@icloud.com>
2026-04-04 09:36:46 +0200

    Added statements to create db - in progress

--- src/main/java/dev/stemvault/db/DatabaseManager.java
+++ src/main/java/dev/stemvault/db/DatabaseManager.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 import java.sql.*;
 
 public class DatabaseManager {
-
+    private static final String DB_CREATE_TABLE = "";
     private static final String USER_HOME = System.getProperty("user.home");
     private static final String STEMVAULT_DB =
         USER_HOME + "/.stemvault/catalog.db";
@@ -16,4 +16,9 @@ public class DatabaseManager {
         Files.createDirectories(path.getParent());
         return DriverManager.getConnection("jdbc:sqlite:" + STEMVAULT_DB);
     }
+    private void initialize(Connection connect) throws SQLException {
+        Statement statement = connect.createStatement();
+
+    }
+
 }
